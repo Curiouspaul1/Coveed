2c3f88105f49486107a13b538a94c35db760572d
Curiouspaul1 <170408025@live.unilag.edu.ng>
2020-05-26 20:50:39 +0100

    updated config file

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ class TestingConfig(Config):
 
 class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
-    FLASK_ENV='development'
+    FLASK_ENV='production'
 
 
 config = {
