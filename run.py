573ff903379be61d1a617f72010a0ae803a1e061
Curious Paul <170408025@live.unilag.edu.ng>
2020-07-16 05:56:55 +0100

    fixed bug

--- run.py
+++ run.py
@@ -8,7 +8,8 @@ import firebase_admin
 
 app = __call__(os.getenv("FLASK_CONFIG") or "default")
 migrate = Migrate(app,db)
-firebase_admin.initialize_app(app.config['CRED'])
+#firebase_admin.initialize_app(app.config['CRED'])
+firebase_admin.initialize_app()
 
 @app.shell_context_processor
 def make_shell_context():
