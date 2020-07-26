511813cd7a7571a7934a6f93184ef5be65cf6455
Bashorun97 <bashorun.emma@gmail.com>
2020-07-26 14:33:45 +0100

    Allow tests run

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ basedir = os.getcwd()
 
 class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    KEY = os.environ['APP_KEY']
+    #KEY = os.environ['APP_KEY']
     ADMIN_EMAIL = ''
     APP_EMAIL = os.environ.get('APP_EMAIL')
     AGENT_EMAILS = os.environ.get('AGENT_EMAILS')
