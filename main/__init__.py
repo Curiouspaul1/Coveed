647a958a150bc0d79dad440bdccad4b6c45e694a
Curious Paul <170408025@live.unilag.edu.ng>
2020-08-20 23:50:07 +0100

    revert cors settings

--- main/__init__.py
+++ main/__init__.py
@@ -12,7 +12,7 @@ def __call__(config_object):
 
     db.init_app(app)
     ma.init_app(app)
-    cors.init_app(app, resources={r"/api/*":{"origins":"https://coveed.live"},r"/doctors/*":{"origins":"https://coveed.live/doctors"}})
+    cors.init_app(app, resources={r"/api/*":{"origins":"*"},r"/doctors/*":{"origins":"*"}})
 
     # register blurprint
     from .api import api as api_blueprint
